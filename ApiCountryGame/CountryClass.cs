23da298ebfa5d838c77fdf3c15926fe6878c5089
ary <ary@makeenenergy.com>
2025-01-08 13:19:44 +0100

    Quiz Works

--- ApiCountryGame/CountryClass.cs
+++ ApiCountryGame/CountryClass.cs
@@ -7,11 +7,19 @@ public class CountryClass
     public List<string> tld { get; set; } // Top Level Domain
     public List<string> capital { get; set; } // Capital of the country (array)
     public List<string> altSpellings { get; set; } // The 2 letter version of a country. Denmark = DK (array)
-    public int population { get; set; } // The population of a country
+    public Flags flags { get; set; }
+    public string population { get; set; } // The population of a country
+    public string area { get; set; } // The countrys size in m2
 }
 
 public class NameInfo
 {
     public string common { get; set; }
     public string official { get; set; }
+}
+
+public class Flags
+{
+    public string png { get; set; }
+    public string svg { get; set; }
 }
