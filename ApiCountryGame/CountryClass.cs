6b88ae9960e680b2bc063f5f2737d9953df41a30
ary <ary@makeenenergy.com>
2025-01-08 09:37:34 +0100

    Json Class Fixed

--- ApiCountryGame/CountryClass.cs
+++ ApiCountryGame/CountryClass.cs
@@ -4,15 +4,14 @@ public class CountryClass
 {
     public string id { get; set; } // A long set of characters used to identify a country
     public NameInfo name { get; set; } // A nested struct with common and official name
-    public string tld { get; set; } // Top Level Domain
-    public string capital { get; set; } // Capital of the country
-    public string altSpellings { get; set; } // The 2 letter version of a country. Denmark = DK
+    public List<string> tld { get; set; } // Top Level Domain
+    public List<string> capital { get; set; } // Capital of the country (array)
+    public List<string> altSpellings { get; set; } // The 2 letter version of a country. Denmark = DK (array)
     public int population { get; set; } // The population of a country
 }
 
 public class NameInfo
 {
-    public string Common { get; set; }
-    public string Official { get; set; }
-    
+    public string common { get; set; }
+    public string official { get; set; }
 }
